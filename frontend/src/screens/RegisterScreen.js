3213c27a514aac2ce849a33cb859a94dbc285557
reddangerous <dkinyanjui144@gmail.com>
2023-12-18 14:31:24 +0300

    otp bug

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -8,6 +8,9 @@ import FormContainer from '../components/FormContainer'
 import { register, sendEmailSend, authEmail } from '../actions/userActions'
 
 
+
+
+
 const RegisterScreen = ({ location, history }) => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
@@ -15,6 +18,9 @@ const RegisterScreen = ({ location, history }) => {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [message, setMessage] = useState(null)
+  const [isSubmitDisabled, setisSubmitDisabled] = useState(true)
+  const [retryCountdown, setRetryCountdown] = useState(60);
+  const [isCountdownActive, setIsCountdownActive] = useState(false);
 
   const dispatch = useDispatch()
   const userRegister = useSelector((state) => state.userRegister)
@@ -34,19 +40,47 @@ const RegisterScreen = ({ location, history }) => {
       setMessage('Passwords do not match')
     } else {
       dispatch(register(name, email, password))
-      dispatch(authEmail(email, otp))
+      setisSubmitDisabled(false)
     }
   }
 const sendOtp = (e) => {
     e.preventDefault()
     dispatch(sendEmailSend(email))
+    setMessage('Otp sent to your email')
+    setIsCountdownActive(true);
   }
+const checkOtp = () => {
+ const otpCorrect = dispatch(authEmail(email, otp));
+
+ if(otpCorrect){
+  setisSubmitDisabled(false)
+  setMessage('Otp is correct')
+  setisSubmitDisabled(false)
+  return
+ }
+  else{ 
+  setMessage('Otp is incorrect please retry or resend otp')
+  setisSubmitDisabled(true)
+  }
+}
+useEffect(() => {
+  const interval = setInterval(() => {
+    setRetryCountdown((prevCountdown) => (prevCountdown > 0 ? prevCountdown - 1 : 0));
+  }, 1000);
+
+  return () => {
+    clearInterval(interval);
+    setisSubmitDisabled(false); 
+  };
+}, [isCountdownActive]);
+
 
 
   return (
     <FormContainer>
+      
       <h1>Sign Up</h1>
-      {message && <Message variant='danger'>{message}</Message>}
+      {message && <Message variant='success'>{message}</Message>}
       {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
@@ -62,23 +96,44 @@ const sendOtp = (e) => {
 
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-          <Button onClick={sendOtp}  variant='primary'> Send Otp</Button>
+          <Row>
+            <Col>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Col>
+            <Col>
+            {isSubmitDisabled && (
+              <Button onClick={sendOtp}>Send OTP</Button>
+            )}
+            </Col>
+          </Row>
+          {retryCountdown > 0 && (
+            <p>{`Didn't receive code? Retry in ${retryCountdown}s`}</p>
+          )}
         </Form.Group>
         <Form.Group controlId='otp'>
+          
           <Form.Label> Otp</Form.Label>
+          <Row>
+            <Col>
           <Form.Control
             type='text'
             placeholder='Enter otp'
             value={otp}
             onChange={(e) => setOTP(e.target.value)}
           ></Form.Control>
+          </Col>
+          <Col>
+          <Button onClick={checkOtp}> verify Otp</Button>
+          </Col>
+          </Row>
         </Form.Group>
+        {isSubmitDisabled && (
+          <>
         <Form.Group controlId='password'>
           <Form.Label>Password Address</Form.Label>
           <Form.Control
@@ -102,8 +157,10 @@ const sendOtp = (e) => {
         <Button type='submit' variant='primary'>
           Register
         </Button>
+        </>
+        )}
       </Form>
-
+      
       <Row className='py-3'>
         <Col>
           Have an Account?{' '}
@@ -112,6 +169,7 @@ const sendOtp = (e) => {
           </Link>
         </Col>
       </Row>
+     
     </FormContainer>
   )
 }
