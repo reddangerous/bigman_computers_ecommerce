468b3742add98c036425289bc5fa7a6fcbca60b2
reddangerous <dkinyanjui144@gmail.com>
2024-01-11 11:36:00 +0300

    color change

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -32,7 +32,7 @@ const CartScreen = ({ match, location, history }) => {
   return (
     <Row>
       <Col md={8}>
-        <h1>Shopping Cart</h1>
+        <h1 className='text-center'>Shopping Cart</h1>
         {cartItems.length === 0 ? (
           <Message>
             Your cart is empty <Link to='/'>Go Back</Link>
@@ -48,7 +48,7 @@ const CartScreen = ({ match, location, history }) => {
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>{item.price} Ksh</Col>
                   <Col md={2}>
                     <Form.Control
                       as='select'
@@ -70,6 +70,7 @@ const CartScreen = ({ match, location, history }) => {
                     <Button
                       type='button'
                       variant='light'
+                      style={{color: '#f0b90b',}}
                       onClick={() => removeFromCartHandler(item.product)}
                     >
                       <i className='fas fa-trash'></i>
@@ -84,15 +85,15 @@ const CartScreen = ({ match, location, history }) => {
       <Col md={4}>
         <Card>
           <ListGroup variant='flush'>
-            <ListGroup.Item>
+            <ListGroup.Item className='text-center text-bold'>
               <h2>
                 Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                 items
               </h2>
-              $
+  
               {cartItems
                 .reduce((acc, item) => acc + item.qty * item.price, 0)
-                .toFixed(2)}
+                .toFixed(2)} Ksh
             </ListGroup.Item>
             <ListGroup.Item>
               <Button
@@ -100,6 +101,7 @@ const CartScreen = ({ match, location, history }) => {
                 className='btn-block'
                 disabled={cartItems.length === 0}
                 onClick={checkoutHandler}
+                style={{backgroundColor: '#f0b90b',}}
               >
                 Proceed To Checkout
               </Button>
